5884bcca5442bad82faea77392ff851272f480fc
William McClay <wilmccla@uat.edu>
2019-06-26 17:15:46 -0700

    Started project, got a bunch of free assets to play around with

---
+++ Surviveulator/Assets/Scripts/Crafting System/CraftingRecipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct ItemAmount
+{
+    //public Item item;
+    public int Amount;
+}
+
+[CreateAssetMenu]
+public class CraftingRecipe : ScriptableObject
+{
+    public List<ItemAmount> materials;
+    public List<ItemAmount> results;
+}
